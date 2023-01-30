4887c2693dffdf8a5017502e5c4f4a881bec0fb8
ruruda <daffarahardian32@gmail.com>
2023-01-30 16:20:57 +0700

    edit respon

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -5,7 +5,7 @@ const create = async (req, res) => {
 	const { nopeg, name, email, password, noHp, roleId } = req.body;
 	const hashPassword = await bcrypt.hash(password, 10);
 	try {
-		if (!nopeg || !name || !email || !password || !noHp || !roleId) {
+		if (!nopeg || !name || !email || !password || !noHp) {
 			return res.status(400).json({
 				message: 'All input is required',
 			});
@@ -13,31 +13,34 @@ const create = async (req, res) => {
 		const userExists = await registrationService.FindUserEmail(email);
 		if (userExists) {
 			return res.status(409).json({
-				message: 'User already exists',
+				message: 'Email already exists',
 			});
 		}
-		// const user = await registrationService.CreateUser({
-		// 	nopeg,
-		// 	name,
-		// 	email,
-		// 	password: hashPassword,
-		// 	noHp,
-		// 	roleId,
-		// });
+		const user = await registrationService.CreateUser({
+			nopeg,
+			name,
+			email,
+			password: hashPassword,
+			noHp,
+			roleId,
+		});
+		const role = await user.getRole();
 		const data = {
 			nopeg,
 			name,
 			email,
 			noHp,
-			roleId,
+			role: role.roles,
 		};
-		return res.status(500).json({
-			message: 'Create success',
+		return res.status(201).json({
+			message: 'User has been created',
 			data,
 		});
 	} catch (err) {
-		console.log(err);
-		return res.status(500).json(err);
+		return res.status(500).json({
+			message: 'Something went wrong',
+			serverMessage: err,
+		});
 	}
 };
 
