5b4e73c7d1f8c0be0a81694a71b8a275c53d0be0
ruruda <daffarahardian32@gmail.com>
2023-02-01 08:22:11 +0700

    menambahkan routes

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
+const adminController = require('../controllers/adminController');
 
 //! User
 router.post('/auth/register', authController.create);
+router.post('/auth/login', authController.login);
+
+//! Admin
+router.get('/admin/users', adminController.getAllUsers);
 
 module.exports = router;
