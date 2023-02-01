6a35a14084c1cd075ea68d934d8b736ae7b8a66e
ruruda <daffarahardian32@gmail.com>
2023-02-01 19:19:06 +0700

    fix req.body.statusHadir

--- server/api/controllers/kehadiranController.js
+++ server/api/controllers/kehadiranController.js
@@ -5,12 +5,15 @@ const updateKehadiran = async (req, res, next) => {
 	try {
 		const getUser = await registrationService.FindUserEmail(req.user.email);
 		await Kehadiran.update(
-			{ [req.body.fieldName]: [req.body.statusHadir] },
+			{ [req.body.fieldName]: req.body.statusHadir },
 			{ where: { userId: getUser.id } }
 		);
-		return res.json('Success');
+		return res.status(200).json({ message: 'Berhasil melakukan presensi' });
 	} catch (err) {
-		return res.json(err);
+		return res.status(500).json({
+			message: 'Something went wrong',
+			serverMessage: err,
+		});
 	}
 };
 
