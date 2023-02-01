f24c0db0fe953717c845a7d01fabfd4d36422ad1
ruruda <daffarahardian32@gmail.com>
2023-02-01 15:04:06 +0700

    menambah kehadiran, user, dan middleware

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
 const adminController = require('../controllers/adminController');
+const kehadiranController = require('../controllers/kehadiranController');
+const auth = require('../middleware/auth');
 
 //! User
 router.post('/auth/register', authController.create);
 router.post('/auth/login', authController.login);
+router.post('/auth/refresh', authController.getRefresh);
+router.get('/auth/me', auth.verifyUser, authController.getMe);
 
 //! Admin
-router.get('/admin/users', adminController.getAllUsers);
+router.get('/admin/users', auth.verifyAdmin, adminController.getAllUsers);
+
+//! Kehadiran
+router.put('/kehadiran/update', auth.verifyUser, kehadiranController.updateKehadiran);
 
 module.exports = router;
