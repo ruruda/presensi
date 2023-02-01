4ea256ab25e3f9f1bb33f4a9380314352c4e5342
ruruda <daffarahardian32@gmail.com>
2023-02-01 15:01:23 +0700

    ganti nama registration service

---
+++ server/api/services/registrations.service.js
@@ -0,0 +1,34 @@
+const { User, Kehadiran } = require('../../models');
+
+const FindUserEmail = async (email) => {
+	return await User.findOne({ where: { email: email } });
+};
+
+const CreateUser = async ({ nopeg, name, email, password, noHp, roleId }) => {
+	return await User.create(
+		{
+			nopeg,
+			name,
+			email,
+			password,
+			noHp,
+			roleId,
+		},
+		{ validate: true }
+	);
+};
+
+const CreateKehadiran = async ({ userId }) => {
+	return await Kehadiran.create(
+		{
+			userId,
+		},
+		{ validate: true }
+	);
+};
+
+module.exports = {
+	FindUserEmail,
+	CreateUser,
+	CreateKehadiran,
+};
