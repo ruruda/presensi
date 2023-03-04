b4452a46caa6a03acef7c0c3a4ee1a4753b13b34
ruruda <daffarahardian32@gmail.com>
2023-03-04 21:49:34 +0700

    add user service delete user

--- server/api/services/user.service.js
+++ server/api/services/user.service.js
@@ -1,9 +1,34 @@
+const { where } = require('sequelize');
 const { User } = require('../../models');
 
 const GetAllUsers = async () => {
 	return await User.findAll();
 };
 
+const DeleteUser = async (uuid) => {
+	return await User.destroy({ where: { uuid: uuid } });
+};
+
+const GetUserById = async (uuid) => {
+	return await User.findOne({ where: { uuid: uuid } });
+};
+
+const UpdateUser = async ({ uuid, nopeg, name, email, password, noHp }) => {
+	return await User.update(
+		{
+			nopeg,
+			name,
+			email,
+			password,
+			noHp,
+		},
+		{ where: { uuid: uuid }, validate: true }
+	);
+};
+
 module.exports = {
 	GetAllUsers,
+	DeleteUser,
+	GetUserById,
+	UpdateUser,
 };
