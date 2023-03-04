030b1743213e9725bc9f6399c0761fd122aa6c9f
ruruda <daffarahardian32@gmail.com>
2023-03-04 21:42:23 +0700

    add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 const mainRoute = require('./api/routes/routes');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', mainRoute);
 
